c5e48d2822961696cc65d376eb82d232e97c4350
TPolyRay <141292446+TPolyRay@users.noreply.github.com>
2024-01-17 16:55:13 +0800

    haiz

---
+++ Project/Server/Configuration/Entities/PublisherSeedConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project.Server.Data;
+using Project.Shared.Domain;
+
+namespace Project.Server.Configurations.Entities
+{
+    public class PublisherSeedConfiguration : IEntityTypeConfiguration<Publisher>
+    {
+        public void Configure(EntityTypeBuilder<Publisher> builder)
+        {
+            builder.HasData(
+                new Publisher
+                {
+                    PublisherId = 1,
+                    PublisherName = "Educational Publishing House",
+                    ContactInfo = "64629603",
+                    EmailAddress = "eph@popularworld.com",
+                    Address = "15 Serangoon North Avenue 5, Singapore 554360",
+                    WebsiteLink = "https://www.eph.com.sg/",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    CreatedBy = "System",
+                    UpdatedBy = "System"
+                }
+                );
+        }
+    }
+}
