50015f4a17f6a91f72bf0354f854b2e0638c89a9
TPolyRay <141292446+TPolyRay@users.noreply.github.com>
2024-01-17 14:51:25 +0800

    ez

--- Project/Server/Controllers/AuthorsController.cs
+++ Project/Server/Controllers/AuthorsController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using P.Server.IRepository;
 using Project.Server.Data;
 using Project.Server.IRepository;
 using Project.Shared.Domain;
